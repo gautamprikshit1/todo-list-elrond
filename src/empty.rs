a21222021b13cd016cc30d46c9ebd5588f719a3d
priks <gautamprikshit1@gmail.com>
2022-10-26 08:44:23 +0530

    Essential Changes

--- src/empty.rs
+++ src/empty.rs
@@ -1,7 +1,5 @@
 #![no_std]
 
-use elrond_wasm::types::heap::String;
-
 elrond_wasm::imports!();
 elrond_wasm::derive_imports!();
 
@@ -9,15 +7,15 @@ elrond_wasm::derive_imports!();
 pub trait TodoListContract {
     #[view(getAllItems)]
     #[storage_mapper("todo_list")]
-    fn todo_list(&self) -> VecMapper<TodoItem>;
+    fn todo_list(&self) -> VecMapper<TodoItem<Self::Api>>;
 
     #[view]
-    fn todo_item(&self, id: u64) -> TodoItem {
+    fn todo_item(&self, id: u64) -> TodoItem<Self::Api> {
         self.todo_list().get(id as usize)
     }
 
     #[init]
-    fn init(&self, description: String, priority: Priority) {
+    fn init(&self, description: ManagedBuffer, priority: Priority) {
         self.todo_list().push(&TodoItem {
             id: 1,
             description,
@@ -27,7 +25,7 @@ pub trait TodoListContract {
     }
 
     #[endpoint]
-    fn add_item(&self, description: String, priority: Priority) {
+    fn add_item(&self, description: ManagedBuffer, priority: Priority) {
         self.todo_list().push(&TodoItem {
             id: self.todo_list().len() as u64 + 1,
             description,
@@ -40,16 +38,16 @@ pub trait TodoListContract {
     fn update_item(
         &self,
         id: u64,
-        description: Option<String>,
-        status: Option<Status>,
-        priority: Option<Priority>,
+        description: OptionalValue<ManagedBuffer>,
+        status: OptionalValue<Status>,
+        priority: OptionalValue<Priority>,
     ) {
         let entry = self.todo_list().get(id as usize);
         let updated_entry = TodoItem {
             id,
-            description: description.unwrap_or(entry.description),
-            status: status.unwrap_or(entry.status),
-            priority: priority.unwrap_or(entry.priority),
+            description: description.into_option().unwrap_or(entry.description),
+            status: status.into_option().unwrap_or(entry.status),
+            priority: priority.into_option().unwrap_or(entry.priority),
         };
         self.todo_list().set(id as usize, &updated_entry);
     }
@@ -61,9 +59,9 @@ pub trait TodoListContract {
 }
 
 #[derive(TopEncode, TopDecode, NestedDecode, NestedEncode, TypeAbi, PartialEq, Clone)]
-pub struct TodoItem {
+pub struct TodoItem<M: ManagedTypeApi> {
     pub id: u64,
-    pub description: String,
+    pub description: ManagedBuffer<M>,
     pub status: Status,
     pub priority: Priority,
 }
